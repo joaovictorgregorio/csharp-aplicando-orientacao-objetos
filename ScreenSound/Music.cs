3d6a6e86099ac6cbf03f8ea04fedc85c0aab6642
João Victor Gregório da Silva <joaogregoriodsilva@gmail.com>
2026-03-09 20:16:08 -0300

    chore: adiciona estrutura inicial do app de console ScreenSound
    - Adiciona arquivos de solução, projeto e .gitignore
    - Implementa classe Music com propriedades Name, Artistic, Duration e Available
    - Adiciona método ViewTechnicalSpecifications para exibir detalhes da faixa
    - Demonstra instanciação e exibição de um objeto Music no Program.cs

---
+++ ScreenSound/Music.cs
@@ -0,0 +1,26 @@
+﻿namespace ScreenSound
+{
+    class Music
+    {
+        public string? Name { get; set; }
+        public string? Artistic { get; set; }
+        public int Duration { get; set; }
+        public bool Available { get; set; }
+        public string? BriefDescription => $"A música {Name} pertence à banda {Artistic}";
+
+
+        #region VIEW TECHNICAL SPECIFICATIONS
+        public void ViewTechnicalSpecifications()
+        {
+            Console.WriteLine($"Nome: {Name}");
+            Console.WriteLine($"Artista: {Artistic}");
+            Console.WriteLine($"Duração: {Duration} segundos");
+
+            if (Available == true) Console.WriteLine("Disponível no plano!");
+            else Console.WriteLine("Assina o plano Plus");
+
+            Console.WriteLine($"{BriefDescription}");
+        }
+        #endregion VIEW TECHNICAL SPECIFICATIONS
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,10 @@
+﻿using ScreenSound;
+
+Console.Clear();
+
+Music music1 = new Music();
+music1.Name  = "Roxane";
+music1.Artistic = "The Police";
+music1.Duration = 273;
+music1.Available = true;
+music1.ViewTechnicalSpecifications();
